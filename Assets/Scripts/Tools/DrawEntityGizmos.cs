5fd63fd3155f1cf6e251148d4c0ed4d9e48714bf
Akin Can HANCER <akncntr57@gmail.com>
2024-10-27 11:00:35 +0300

    feat: Create draw gizmos class.

---
+++ Assets/Scripts/Tools/DrawEntityGizmos.cs
@@ -0,0 +1,32 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Tools
+{
+    public class DrawEntityGizmos : MonoBehaviour
+    {
+        [SerializeField] private Color color;
+        [SerializeField] private GizmosTypes type;
+
+        [ShowIf("type", GizmosTypes.Cube)]
+        [SerializeField] private Vector3 center;
+        [ShowIf("type", GizmosTypes.Cube)]
+        [SerializeField] private Vector3 size;
+        
+        private void OnDrawGizmos()
+        {
+            // switch (type)
+            // {
+            //     case GizmosTypes.Cube:
+            //         Gizmos.DrawCube();
+            // }
+        }
+    }
+
+    public enum GizmosTypes
+    {
+        Cube,
+        Sphere,
+        Line
+    }
+}
