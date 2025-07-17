a43864b572d059adbbcbb8450b3b336e5b7f4242
Akın Can HANÇER <akncntr57@gmail.com>
2025-07-18 01:20:15 +0300

    feat: Create statemachine base.

---
+++ Assets/Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+    protected State CurrentState;
+
+    public void ChangeState(State newState)
+    {
+        if (CurrentState != null)
+            CurrentState.Exit();
+
+        CurrentState = newState;
+
+        if (CurrentState != null)
+            CurrentState.Enter();
+    }
+
+    protected virtual void Update()
+    {
+        CurrentState?.Update();
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        CurrentState?.FixedUpdate();
+    }
+}
