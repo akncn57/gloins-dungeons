3c320b0d2836bf565d9353d98ebb95a279226f9e
Akın Can HANÇER <akncntr57@gmail.com>
2026-03-21 19:30:09 +0300

    *

--- Assets/Scripts/Character/CharacterStateMachine.cs
+++ Assets/Scripts/Character/CharacterStateMachine.cs
@@ -1,16 +1,29 @@
-﻿using System;
+﻿using Character.States;
+using StateMachine;
 
 namespace Character
 {
-    public class CharacterStateMachine : StateMachine
+    public class PlayerStateMachine : BaseStateMachine<CharacterController>
     {
-        public CharacterIdleState IdleState { get; private set; }
-        public CharacterWalkState WalkState { get; private set; }
+        public IdleState IdleState { get; }
+        public WalkState WalkState { get; }
+        public DashState DashState { get; }
+        public LightAttackState LightAttackState { get; }
+        public HeavyAttackState HeavyAttackState { get; }
+        public HurtState HurtState { get; }
+        public DeathState DeathState { get; }
         
-        private void Awake()
+        public PlayerStateMachine(CharacterController context)
         {
-            IdleState = new CharacterIdleState(this);
-            WalkState = new CharacterWalkState(this);
+            IdleState = new IdleState(context, this);
+            WalkState = new WalkState(context, this);
+            DashState = new DashState(context, this);
+            LightAttackState = new LightAttackState(context, this);
+            HeavyAttackState = new HeavyAttackState(context, this);
+            HurtState = new HurtState(context, this);
+            DeathState = new DeathState(context, this);
+            
+            Initialize(IdleState);
         }
     }
 }
---
+++ Assets/Scripts/StateMachine/BaseStateMachine.cs
@@ -0,0 +1,30 @@
+﻿namespace StateMachine
+{
+    public abstract class StateMachine<T>
+    {
+        public State<T> CurrentState { get; protected set; }
+    
+        public void Initialize(State<T> startingState)
+        {
+            CurrentState = startingState;
+            CurrentState?.Enter();
+        }
+
+        public void ChangeState(State<T> newState)
+        {
+            CurrentState?.Exit();
+            CurrentState = newState;
+            CurrentState?.Enter();
+        }
+
+        public virtual void Update()
+        {
+            CurrentState?.Update();
+        }
+
+        public virtual void FixedUpdate()
+        {
+            CurrentState?.FixedUpdate();
+        }
+    }
+}
